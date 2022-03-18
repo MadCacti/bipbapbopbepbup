facabdb86bd62cd411332537760559f883979c8b
Raiden <45216311+MadCacti@users.noreply.github.com>
2022-03-18 07:04:34 +0000

    create database(hack1/2)

---
+++ procedures/database.py
@@ -0,0 +1,25 @@
+InfoDb = []
+
+InfoDb.append({  
+               "Name": "Raiden Tung",  
+               "Birthday": "January 15",  
+               "Classes Taking":["US History", "AM Lit", "AP Physics", "AP Stats", "AP CSP"]  
+              })  
+
+InfoDb.append({  
+               "Name": "Audrey Zeng",  
+               "Birth": "July 22",  
+               "Classes Taking":["AP Studio Art","APEL","APUSH","AP Stats", "AP Bio"] 
+              })
+
+InfoDb.append({  
+               "Name": "Jeffrey Lee",  
+               "Birth": "December 27",  
+               "Classes Taking":["AP Physics","AP Calc BC", "US History", "AM Lit", "AP Music Theory"] 
+              })
+
+InfoDb.append({  
+               "Name": "Muhan Wei",  
+               "Birth": "February 22",  
+               "Classes Taking":["US History","Racquet Sports","Pre-Calc","AP Physics", "Offroll"] 
+              })
