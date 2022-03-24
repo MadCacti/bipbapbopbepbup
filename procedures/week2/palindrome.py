37b459c08e59b03a5333728203d5577446e3367c
Raiden <45216311+MadCacti@users.noreply.github.com>
2022-03-24 16:11:08 +0000

    add spacing, looks nice

--- procedures/week2/palindrome.py
+++ procedures/week2/palindrome.py
@@ -4,11 +4,13 @@ inverse = ""
 # all the special chars: need to filter them out + filter out spaces- !@#$%^&*()`~-_+=[{]}\|;:"',<.>/?'"]
 for phrases in phrase:
   print("original phrase:", phrases)
+  
   for chars in specialchars:
     phrases = phrases.replace(chars, "")
     inverse = phrases[::-1] # this reverses string
     phrases.lower()
   print("new phrase:", phrases)  
+  
   if (phrases == inverse):
     print(phrases, "is a palindrome")
   else:
