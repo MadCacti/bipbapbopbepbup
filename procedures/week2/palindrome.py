bc57dbef38e549cb36d78eb65671caffe1169193
Raiden <45216311+MadCacti@users.noreply.github.com>
2022-03-28 19:35:24 +0000

    palindrome fix

--- procedures/week2/palindrome.py
+++ procedures/week2/palindrome.py
@@ -1,17 +1,35 @@
-phrase = ["racecar", "raCe caR!>#", "wrong"] # tests all different types: normal, abnormal, and example of incorrect
-specialchars = "!@#$%^&*()~`<>,./?':;}{][\| "
-inverse = ""
-# all the special chars: need to filter them out + filter out spaces- !@#$%^&*()`~-_+=[{]}\|;:"',<.>/?'"]
-for phrases in phrase:
-  print("original phrase:", phrases)
-  
-  for chars in specialchars:
-    phrases = phrases.replace(chars, "")
-    phrases = phrases.lower()
-    inverse = phrases[::-1] # this reverses string
-  print("new phrase:", phrases)  
-  
-  if (phrases == inverse):
-    print(phrases, "is a palindrome")
-  else:
-    print(phrases, "is not a palindrome")
+class Palindrome:
+    def __init__(self):
+        self.index = 0
+        self.input = ""
+        self.specials = [" ", "-", ",", "!", "?", ".", "$", "*", "#", "%", "@", "`", "~", "<", ">", "{", "}", "[", "]"]
+
+    def clean(self): # removes special characters and forces all characters to lower case
+        self.input = self.input.lower()
+        for element in self.specials:
+            self.input = self.input.replace(element, '')
+        return self.input
+
+    def __call__(self, input):
+        self.input = input
+        print("original input:", self.input)
+        self.clean()
+
+        index = len(self.input)
+        palindrome = True
+
+        for i in range(index):
+            if self.input[i] != self.input[::-1][i]: # ::-1 essentially reverses string and the rest compares each letter
+                print("errors occurs at the pairs: " + str(i), str((len(self.input) - i)))
+                palindrome = False
+
+        if palindrome == False:
+            print(self.input ,"is not a palindrome")
+        else:
+            print(self.input, " is a palindrome")
+
+
+pal = Palindrome()
+pal("racecar")
+pal("wrineerw")
+pal("raCe caR!>#")
